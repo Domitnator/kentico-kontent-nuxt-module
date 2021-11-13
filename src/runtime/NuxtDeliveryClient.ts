c407154875d05ebc8f610ff088d067be0d225132
Alfred Brockotter <alfred@domitnator.nl>
2021-11-13 19:45:48 +0100

    fix: Fixed typescript issue in NuxtdeliveryClient

--- src/runtime/NuxtDeliveryClient.ts
+++ src/runtime/NuxtDeliveryClient.ts
@@ -4,43 +4,43 @@ import { INuxtDeliveryClient } from "./INuxtDeliveryClient";
 
 export class NuxtDeliveryClient implements INuxtDeliveryClient{
     mappingService: IMappingService;
-    #deliveryClient: IDeliveryClient;
-    #cacheService: CacheService;
+    deliveryClient: IDeliveryClient;
+    cacheService: CacheService;
     constructor(config: IDeliveryClientConfig){
-        this.#deliveryClient = new DeliveryClient(config);
-        this.#cacheService = new CacheService(this.#deliveryClient);
+        this.deliveryClient = new DeliveryClient(config);
+        this.cacheService = new CacheService(this.deliveryClient);
     }
     viaCache<TItem extends ContentItem>(query: MultipleItemQuery<TItem>, seconds: number, cacheKey?: string, isServerProcess?: boolean): Promise<ItemResponses.ListContentItemsResponse<TItem>> {
-        return this.#cacheService.viaCache(query, seconds, cacheKey, isServerProcess);
+        return this.cacheService.viaCache(query, seconds, cacheKey, isServerProcess);
     }
     languages(): LanguagesQuery {
-       return this.#deliveryClient.languages();
+       return this.deliveryClient.languages();
     }
     types(): MultipleTypeQuery {
-        return this.#deliveryClient.types();
+        return this.deliveryClient.types();
     }
     type(typeCodename: string): SingleTypeQuery {
         return this.type(typeCodename);
     }
     items<TItem extends ContentItem>(): MultipleItemQuery<TItem> {
-        return this.#deliveryClient.items<TItem>();
+        return this.deliveryClient.items<TItem>();
     }
     itemsFeed<TItem extends ContentItem>(): ItemsFeedQuery<TItem> {
-        return this.#deliveryClient.itemsFeed<TItem>();
+        return this.deliveryClient.itemsFeed<TItem>();
     }
     itemsFeedAll<TItem extends ContentItem>(): ItemsFeedQueryAll<TItem> {
-        return this.#deliveryClient.itemsFeedAll<TItem>();
+        return this.deliveryClient.itemsFeedAll<TItem>();
     }
     item<TItem extends ContentItem>(codename: string): SingleItemQuery<TItem> {
-        return this.#deliveryClient.item<TItem>(codename);
+        return this.deliveryClient.item<TItem>(codename);
     }
     taxonomies(): TaxonomiesQuery {
-        return this.#deliveryClient.taxonomies();
+        return this.deliveryClient.taxonomies();
     }
     taxonomy(codename: string): TaxonomyQuery {
-        return this.#deliveryClient.taxonomy(codename);
+        return this.deliveryClient.taxonomy(codename);
     }
     element(typeCodename: string, elementCodename: string): ElementQuery {
-        return this.#deliveryClient.element(typeCodename, elementCodename);
+        return this.deliveryClient.element(typeCodename, elementCodename);
     }
 }
